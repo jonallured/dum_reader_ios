b05c6a418c259a24b5a3fcd8c7eb13484bb52698
Jon Allured <jon.allured@gmail.com>
2017-11-01 15:46:33 -0500

    Extract store to manage entries

--- Dum Reader/Controllers/EntriesController.swift
+++ Dum Reader/Controllers/EntriesController.swift
@@ -5,31 +5,23 @@ class EntriesController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var segmentControl: UISegmentedControl!
 
-    var entries = [Entry]()
+    var entryStore = EntryStore()
+
+    var entries: [Entry] {
+        return entryStore.entries
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        Router.hit(.unreadEntries, handler: unreadEntriesHandler)
-
         tableView.delegate = self
         tableView.dataSource = self
+        entryStore.delegate = self
+        entryStore.load()
     }
+}
 
-    private func unreadEntriesHandler(data: Data?, response: URLResponse?, error: Error?) {
-        guard let data = data,
-            let entryIds = try? JSONDecoder().decode([Int].self, from: data)
-            else { fatalError() }
-
-        Router.hit(.entries(ids: entryIds), handler: entriesHandler)
-    }
-
-    private func entriesHandler(data: Data?, response: URLResponse?, error: Error?) {
-        guard let data = data,
-            let entries = try? JSONDecoder.apiDecoder().decode([Entry].self, from: data)
-            else { fatalError() }
-
-        self.entries = entries
+extension EntriesController: EntryStoreDelegate {
+    func didLoadEntries() {
         DispatchQueue.main.async(execute: tableView.reloadData)
     }
 }
@@ -63,21 +55,5 @@ extension EntriesController: UITableViewDataSource {
 extension EntriesController: SFSafariViewControllerDelegate {
     func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
         controller.dismiss(animated: true, completion: nil)
-    }
-}
-
-private extension DateFormatter {
-    class func apiDateFormatter() -> DateFormatter {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-        return formatter
-    }
-}
-
-private extension JSONDecoder {
-    class func apiDecoder() -> JSONDecoder {
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .formatted(DateFormatter.apiDateFormatter())
-        return decoder
     }
 }
---
+++ Dum Reader/Models/EntryStore.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+protocol EntryStoreDelegate {
+    func didLoadEntries()
+}
+
+class EntryStore {
+    var delegate: EntryStoreDelegate?
+    var entries = [Entry]()
+
+    func load() {
+        Router.hit(.unreadEntries, handler: unreadEntriesHandler)
+    }
+
+    private func unreadEntriesHandler(data: Data?, response: URLResponse?, error: Error?) {
+        guard let data = data,
+            let entryIds = try? JSONDecoder().decode([Int].self, from: data)
+            else { fatalError() }
+
+        Router.hit(.entries(ids: entryIds), handler: entriesHandler)
+    }
+
+    private func entriesHandler(data: Data?, response: URLResponse?, error: Error?) {
+        guard let data = data,
+            let entries = try? JSONDecoder.apiDecoder().decode([Entry].self, from: data)
+            else { fatalError() }
+
+        self.entries = entries
+        delegate?.didLoadEntries()
+    }
+}
+
+private extension DateFormatter {
+    class func apiDateFormatter() -> DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        return formatter
+    }
+}
+
+private extension JSONDecoder {
+    class func apiDecoder() -> JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(DateFormatter.apiDateFormatter())
+        return decoder
+    }
+}
