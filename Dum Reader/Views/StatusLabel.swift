06844808fa9176952a0a5405b7311ae36494f35a
Jon Allured <jon.allured@gmail.com>
2017-11-02 11:12:47 -0500

    Improve format of timestamp

--- Dum Reader/Views/StatusLabel.swift
+++ Dum Reader/Views/StatusLabel.swift
@@ -4,11 +4,20 @@ class StatusLabel: UILabel {
     var count: Int? {
         didSet {
             if let count = count {
-                let date = Date()
-                text = "\(count) unread as of \(date)"
+                let timestamp = DateFormatter.timestamp().string(from: Date())
+                text = "\(count) unread as of \(timestamp)"
             } else {
                 text = "loading..."
             }
         }
+    }
+}
+
+private extension DateFormatter {
+    class func timestamp() -> DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
     }
 }
